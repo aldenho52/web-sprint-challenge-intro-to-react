95d92d6b5b9ad10eaf053ac85bd2b98c36fe2f16
Alden Ho <aldenho@Aldens-MacBook-Air.local>
2020-10-09 09:33:58 -0700

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,30 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(`https://swapi.py4e.com/api/people`)
+      .then(res => {
+        console.log(res)
+        setData(res.data.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  }, [])
+
+  console.log(data)
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star Wars Characters</h1>
+      {
+        data.map(obj => {
+          return <Character data={obj}/>
+        })
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
-// Write your Character component here
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+const StyledChar = styled.div`
+    background: lightgray;
+    border: 3px solid black;
+    border-radius: 5px;
+    margin: 1% 0;
+    opacity: 50%;
+
+    &:hover {
+        opacity: 100%;
+        background: lightblue;
+        transition: all 0.3s ease-in-out;
+    }
+`
+
+
+const Character = (props) => {
+    const {data} = props
+    return (
+        <StyledChar>
+            <h2>{data.name}</h2>
+            <p>Birth Year: {data.birth_year}</p>
+            <p>Gender: {data.gender}</p>
+            <p>Height: {data.height}   Mass: {data.mass}</p>
+        </StyledChar>
+    )
+}
+
+export default Character
